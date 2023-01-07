f44dd1657d3af5351f9e26c82ced68ef6f19c8dd
Laura Ann Seal <lauraannseal@Lauras-Mac-mini.local>
2023-01-07 16:26:15 +0200

    init commit

---
+++ header.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package DOYOURWORST
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<!-- bootstrap stylesheet -->
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/bootstrap/bootstrap.css">
+
+	<!-- fontawesome -->
+	<script src="https://kit.fontawesome.com/d86f6a05aa.js" crossorigin="anonymous"></script>
+
+	<!-- social meta -->
+	<?php get_template_part('template-parts/social', 'meta') ?>
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'doyourworst' ); ?></a>
+
+	
---
+++ template-parts/social-meta.php
@@ -0,0 +1,18 @@
+<!-- HTML Meta Tags -->
+<title>Do Your Worst</title>
+<meta name="description" content="Coming soon...">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://equationsforabodyatrest.com/">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Equations for a body at rest">
+<meta property="og:description" content="Coming soon...">
+<meta property="og:image" content="<?php echo get_template_directory_uri() ?>/assets/images/og-do-your-worst.jpg">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="equationsforabodyatrest.com">
+<meta property="twitter:url" content="https://equationsforabodyatrest.com/">
+<meta name="twitter:title" content="Equations for a body at rest">
+<meta name="twitter:description" content="Coming soon...">
+<meta name="twitter:image" content="<?php echo get_template_directory_uri() ?>/assets/images/twitter-do-your-worst.jpg">
